a566c97dd85f80314491aa1eb6f2011103784273
Kenni Woodard <kennikristine@gmail.com>
2023-01-30 15:58:24 -0800

    reorganized some files, homepage redirects to auth if not signed in, but there are console errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { UserContext } from './context/UserContext.js';
+import Header from './components/Header.js';
+import { useContext } from 'react';
+import Auth from './components/Auth/Auth.js';
+import Duties from './components/Duties/Duties.js';
 
 function App() {
-  return <div className="App">Dreamlight Valley Duties</div>;
+  const { user } = useContext(UserContext);
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route path="/auth/:type" component={Auth} />
+        <Route path="/duties" component={Duties} />
+        <Route exact path="/">
+          <>
+            {user && <Redirect to="/duties" />}
+            {!user && <Redirect to="/auth/sign-in" />}
+          </>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/context/userContext.js
@@ -0,0 +1,22 @@
+const { createContext, useState } = require('react');
+const { getUser } = require('../services/auth');
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
+
+// const useUser = () => {
+//   const data = useContext(UserContext);
+
+//   if (!data) {
+//     throw new Error('useUser must be wrapped in a UserProvider');
+//   }
+//   return data;
+// };
+
+export { UserProvider, UserContext };
