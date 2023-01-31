dbbda9b20fd6a8e63857ff3fc42721b6ff771e80
Kenni Woodard <kennikristine@gmail.com>
2023-01-30 16:36:55 -0800

    lite nonfunctional auth form built out in Auth.js

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 
 export default function Auth() {
   const [email, setEmail] = useState('');
@@ -9,5 +9,24 @@ export default function Auth() {
     //later
   };
 
-  return <div>hi</div>;
+  return (
+    <div>
+      <nav>
+        <h2>Welcome to Dreamlight Duties Tracker!</h2>
+        <div>
+          <NavLink to="/auth/sign-in">Sign-In</NavLink>
+          <NavLink to="/auth/sign-up">Sign-Up</NavLink>
+        </div>
+        <div>
+          <label>Email</label>
+          <input className="input" type="email"></input>
+          <label>Password</label>
+          <input className="input" type="password"></input>
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </nav>
+    </div>
+  );
 }
